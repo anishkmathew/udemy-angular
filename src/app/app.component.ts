5b9a417d8055b7b5b9dded664fe84e3c4598cf41
anish <anishkmathew@gmail.com>
2018-07-15 22:49:52 +0530

    Components splitting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,18 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  servers = [];
+  serverElements = [];
+  newServerName = '';
+  newServerContent = '';
 
-  onAddServer(){
-    this.servers.push('Another Server');
-
+  onAddServer() {
+    this.serverElements.push({
+      type: 'server',
+      name: this.newServerName,
+      content: this.newServerContent
+    });
   }
 
-  onRemoveServer(id: number){
-    const position = id;
-    console.log(id);
-    
-    this.servers.splice(position,1);
+  onAddBlueprint() {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: this.newServerName,
+      content: this.newServerContent
+    });
   }
-
 }
