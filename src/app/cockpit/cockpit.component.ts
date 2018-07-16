51872e67994606df71b8e65cf1c9b5a614b33295
anish <anishkmathew@gmail.com>
2018-07-17 01:18:12 +0530

    multi level binding, parent to child and child to parent. Binding by Input decorator, Output decorator with Aliasing. Binding by local reference, Binding by viewChild decoratior, View Encapsulation. Event binding by EventEmitter

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,31 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
   templateUrl: './cockpit.component.html',
   styleUrls: ['./cockpit.component.css']
+  
 })
 export class CockpitComponent implements OnInit {
-  newServerName = '';
-  newServerContent = '';
+  @Output('sCreated') serverCreated = new EventEmitter<{serverName:string, serverContent:string}>();
+  @Output('bpCreated') blueprintCreated = new EventEmitter<{blueprintName:string, blueprintContent:string}>();
+  //newServerName = 'Anish Server';
+  //newServerContent = 'Test Content initial';
+  @ViewChild('serverContentInput') serverContentInput:ElementRef;
+ 
   constructor() { }
 
   ngOnInit() {
   }
 
-  onAddServer() {
-    // this.serverElements.push({
-    //   type: 'server',
-    //   name: this.newServerName,
-    //   content: this.newServerContent
-    // });
+  onAddServer(nameInput:HTMLInputElement) {
+    //console.log(nameInput);
+    
+    this.serverCreated.emit({
+      //serverName:this.newServerName,
+      serverName:nameInput.value,
+      //serverContent:this.newServerContent  
+      serverContent:this.serverContentInput.nativeElement.value
+    })
   }
 
-  onAddBlueprint() {
-  //   this.serverElements.push({
-  //     type: 'blueprint',
-  //     name: this.newServerName,
-  //     content: this.newServerContent
-  //   });
+  onAddBlueprint(nameInput:HTMLInputElement) {
+    this.blueprintCreated.emit({
+      blueprintName:nameInput.value,
+     // blueprintContent:this.newServerContent    
+      blueprintContent:this.serverContentInput.nativeElement.value
+    })
   }
 }
